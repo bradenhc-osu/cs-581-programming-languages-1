b2021593a6912b076584b69c7e508c83c687ef3a
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-10-05 19:22:51 -0700

    solution: homework 3 solved

---
+++ HW3.hs
@@ -0,0 +1,30 @@
+module HW3 where
+
+
+-- | Convert a regular list into a run-length list.
+--
+--   >>> compress [1,1,1,2,3,3,3,1,2,2,2,2]
+--   [(3,1),(1,2),(3,3),(1,1),(4,2)]
+-- 
+--   >>> compress "Mississippi"
+--   [(1,'M'),(1,'i'),(2,'s'),(1,'i'),(2,'s'),(1,'i'),(2,'p'),(1,'i')]
+--
+compress :: Eq a => [a] -> [(Int,a)]
+compress xs = compressor (reverse xs) []
+    where
+        compressor :: Eq a => [a] -> [(Int, a)] -> [(Int, a)]
+        compressor [] ts = ts
+        compressor (x:xs) [] = compressor xs [(1,x)]
+        compressor (x:xs) ((ti, tx):ts) = if tx == x 
+            then compressor xs ((1 + ti, tx):ts) 
+            else compressor xs ((1,x) : (ti,tx) : ts)
+
+
+-- | Convert a run-length list back into a regular list.
+--
+--   >>> decompress [(5,'a'),(3,'b'),(4,'c'),(1,'a'),(2,'b')]
+--   "aaaaabbbccccabb"
+--  
+decompress :: [(Int,a)] -> [a]
+decompress [] = []
+decompress ((x,y):rest) = (replicate x y) ++ decompress rest
