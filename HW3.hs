6d8b504fccf9d8bdf2754dad4608d4996285e0ad
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-10-10 16:01:50 -0700

    refactor: better implementation for compress

--- HW3.hs
+++ HW3.hs
@@ -10,14 +10,26 @@ module HW3 where
 --   [(1,'M'),(1,'i'),(2,'s'),(1,'i'),(2,'s'),(1,'i'),(2,'p'),(1,'i')]
 --
 compress :: Eq a => [a] -> [(Int,a)]
-compress xs = compressor (reverse xs) []
+-- compress xs = compressor (reverse xs) []
+--     where
+--         compressor :: Eq a => [a] -> [(Int, a)] -> [(Int, a)]
+--         compressor [] ts = ts
+--         compressor (x:xs) [] = compressor xs [(1,x)]
+--         compressor (x:xs) ((ti, tx):ts) = if tx == x 
+--             then compressor xs ((1 + ti, tx):ts) 
+--             else compressor xs ((1,x) : (ti,tx) : ts)
+compress = map entry . chunk
     where
-        compressor :: Eq a => [a] -> [(Int, a)] -> [(Int, a)]
-        compressor [] ts = ts
-        compressor (x:xs) [] = compressor xs [(1,x)]
-        compressor (x:xs) ((ti, tx):ts) = if tx == x 
-            then compressor xs ((1 + ti, tx):ts) 
-            else compressor xs ((1,x) : (ti,tx) : ts)
+        entry :: [a] -> (Int, a)
+        entry ys = (length ys, head ys)
+
+        chunk :: Eq a => [a] -> [[a]]
+        chunk [] = []
+        chunk (x:xs) = let (match, rest) = all x xs in match : chunk rest
+
+        all :: Eq a => a -> [a] -> ([a], [a])
+        all x [] = ([x], [])
+        all x (y:ys) = if x == y then let (match, rest) = all x ys in (y : match, rest) else ([x], y:ys)
 
 
 -- | Convert a run-length list back into a regular list.
